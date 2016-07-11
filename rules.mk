d3bbcbfe35cdd763976f0172bdd09a44df1abc67
Olivier Detour <detour.olivier@gmail.com>
2016-07-11 15:48:30 +0200

    use builder rules

---
+++ rules.mk
@@ -0,0 +1,12 @@
+_m = dotfiles
+
+$(_m)-links =	\
+  gitconfig	\
+  Xdefaults	\
+  xinitrc	\
+  xsession
+
+$(_m)-link-gitconfig	= ~/.gitconfig
+$(_m)-link-Xdefaults	= ~/.Xdefaults
+$(_m)-link-xinitrc	= ~/.xinitrc
+$(_m)-link-xsession	= ~/.xsession
