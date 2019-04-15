2349e3e366e33e75163c6889987d9b1dfe518c03
Olivier Detour <detour.olivier@gmail.com>
2019-04-15 10:09:53 +0200

    Add increase/decrease font size for urxvt

--- rules.mk
+++ rules.mk
@@ -1,12 +1,21 @@
 _m = dotfiles
 
+$(_m)-dirs =	\
+  urxvt_ext
+
 $(_m)-links =	\
   gitconfig	\
   Xdefaults	\
+  Xresources	\
+  resize_font	\
   xinitrc	\
   xsession
 
+$(_m)-dir-urxvt_ext	= ~/.urxvt/ext
+
 $(_m)-link-gitconfig	= ~/.gitconfig
 $(_m)-link-Xdefaults	= ~/.Xdefaults
+$(_m)-link-Xresources	= ~/.Xresources
+$(_m)-link-resize_font	= ~/.urxvt/ext/resize-font
 $(_m)-link-xinitrc	= ~/.xinitrc
 $(_m)-link-xsession	= ~/.xsession
